0a55891b84594ac720a74b689ec2216f64b3f34e
Alexander Vostres <alexx999@gmail.com>
2013-05-31 12:55:24 +0300

    add some Cyrillic chars to test

--- PcreSharpTest/Program.cs
+++ PcreSharpTest/Program.cs
@@ -57,7 +57,7 @@ namespace PcreSharpTest
 
 		private static void GenerateRandomString(StringBuilder builder, int len)
 		{
-			char[] chars = {
+			char[] chars = {'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у',
 			               	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
 			               	'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
 			               	'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7',
